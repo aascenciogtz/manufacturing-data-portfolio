f3c835581b5f93ae649d0eaf77a52b7b46083130
Alan Ascencio <aascenciogtz@gmail.com>
2026-09-15 14:32:08 -0600

    Update dia-01-fundamentos.sql

--- semana-01/dia-01-fundamentos.sql
+++ semana-01/dia-01-fundamentos.sql
@@ -3,18 +3,13 @@
 -- Temas: SELECT, FROM, LIMIT, AS
 
 -- Ejercicio 1
-SELECT * FROM materiales
-SELECT nombre FROM materiales
-SELECT precio FROM materiales
-SELECT precio FROM materiales LIMIT 5
-SELECT precio FROM materiales LIMIT 2
-SELECT nombre AS "Nombre del Material" FROM materiales LIMIT 3
-SELECT nombre AS "Nombre del Material" FROM materiales LIMIT 5
-SELECT nombre FROM materiales WHERE precio>1200
-SELECT nombre FROM materiales WHERE moneda="USD"
+SELECT * FROM materiales;
+SELECT nombre FROM materiales;
+SELECT precio FROM materiales;
+SELECT precio FROM materiales LIMIT 5;
+SELECT precio FROM materiales LIMIT 2;
+SELECT nombre AS 'Nombre del Material' FROM materiales LIMIT 3;
+SELECT nombre AS 'Nombre del Material' FROM materiales LIMIT 5;
+SELECT nombre FROM materiales WHERE precio>1200;
+SELECT nombre FROM materiales WHERE moneda='USD';
 
-
--- Ejercicio 2
-
-
--- Ejercicio 3
