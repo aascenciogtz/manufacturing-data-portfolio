6c2dc5ab57d0612d091903f38287d1a7c9d6ad54
aascenciogtz <aascenciogtz@gmail.com>
2026-09-15 09:54:37 -0600

    Create fundamentos SQL exercises for Semana 01
    Added SQL exercises for basic SELECT queries.

---
+++ semana-01/dia-01-fundamentos.sql
@@ -0,0 +1,20 @@
+-- Semana 01 - Día 01
+-- Fundamentos SQL
+-- Temas: SELECT, FROM, LIMIT, AS
+
+-- Ejercicio 1
+SELECT * FROM materiales
+SELECT nombre FROM materiales
+SELECT precio FROM materiales
+SELECT precio FROM materiales LIMIT 5
+SELECT precio FROM materiales LIMIT 2
+SELECT nombre AS "Nombre del Material" FROM materiales LIMIT 3
+SELECT nombre AS "Nombre del Material" FROM materiales LIMIT 5
+SELECT nombre FROM materiales WHERE precio>1200
+SELECT nombre FROM materiales WHERE moneda="USD"
+
+
+-- Ejercicio 2
+
+
+-- Ejercicio 3
